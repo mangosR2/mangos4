DeKaDeNcE/mangos-mop

update dbc structures
Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

src/game/DBCfmt.h
@@ -19,25 +19,25 @@
 #ifndef MANGOS_DBCSFRM_H
 #define MANGOS_DBCSFRM_H
 
-const char Achievementfmt[]="niiissiiiiisii";
+const char Achievementfmt[]="niiissiiiiisiix"; // 16357
 const char AchievementCriteriafmt[]="niiiiiiiixsiiiiixxxxxxx";
-const char AreaTableEntryfmt[]="iiinixxxxxisiiiiixxxxxxxxx";
+const char AreaTableEntryfmt[]="iiinixxxxxxxisiiiiixxxxxxxxxxx"; // 16357
 const char AreaGroupEntryfmt[]="niiiiiii";
-const char AreaTriggerEntryfmt[]="nifffxxxfffff";
+const char AreaTriggerEntryfmt[]="nifffxxxfffffxxx"; // 16357
 const char ArmorLocationfmt[]="nfffff";
 const char AuctionHouseEntryfmt[]="niiix";
 const char BankBagSlotPricesEntryfmt[]="ni";
 const char BarberShopStyleEntryfmt[]="nixxxiii";
-const char BattlemasterListEntryfmt[]="niiiiiiiiixsiiiiiiii";
+const char BattlemasterListEntryfmt[]="niiiiiiiiiiixsiiiiiiii"; // 16357
 const char CharStartOutfitEntryfmt[]="diiiiiiiiiiiiiiiiiiiiiiiiixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 const char CharTitlesEntryfmt[]="nxsxix";
 const char ChatChannelsEntryfmt[]="iixsx";
                                                             // ChatChannelsEntryfmt, index not used (more compact store)
-const char ChrClassesEntryfmt[]="nixsxxxixiiiii";
-const char ChrRacesEntryfmt[]="nxixiixixxxxixsxxxxxixxx";
+const char ChrClassesEntryfmt[]="nixsxxxixiiiixxxxx"; // 16357
+const char ChrRacesEntryfmt[]="nxixiixixxxxixsxxxxxixxxxxxxxxxxxxxx"; // 16357
 const char ChrClassesXPowerTypesfmt[]="nii";
 const char CinematicSequencesEntryfmt[]="nxxxxxxxxx";
-const char CreatureDisplayInfofmt[]="nxxifxxxxxxxxxxxx";
+const char CreatureDisplayInfofmt[]="nxxifxxxxxxxxxxxxxx"; // 16357
 const char CreatureDisplayInfoExtrafmt[]="nixxxxxxxxxxxxxxxxxxx";
 const char CreatureFamilyfmt[]="nfifiiiiixsx";
 const char CreatureSpellDatafmt[]="niiiixxxx";
@@ -70,7 +70,7 @@ const char GtSpellScalingfmt[]="df";
 const char GtOCTBaseHPByClassfmt[]="df";
 const char GtOCTBaseMPByClassfmt[]="df";
 const char Holidaysfmt[]="nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-const char ItemClassfmt[]="nixxxs";
+const char ItemClassfmt[]="nxfs"; // 16357
 const char ItemArmorQualityfmt[]="nfffffffi";
 const char ItemArmorShieldfmt[]="nifffffff";
 const char ItemArmorTotalfmt[]="niffff";
@@ -85,9 +85,9 @@ const char ItemSetEntryfmt[]="dsxxxxxxxxxxxxxxxxxiiiiiiiiiiiiiiiiii";
 const char LiquidTypefmt[] = "nxxixixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 const char LockEntryfmt[]="niiiiiiiiiiiiiiiiiiiiiiiixxxxxxxx";
 const char MailTemplateEntryfmt[]="nxs";
-const char MapEntryfmt[]="nsiiiisissififfiiiii";
+const char MapEntryfmt[]="nsiiisissififfiiiii"; // 16357
 const char MapDifficultyEntryfmt[]="niisiis";
-const char MovieEntryfmt[]="nxxx";
+const char MovieEntryfmt[]="nxxxx"; // 16357
 const char NumTalentsAtLevelfmt[]="df";
 const char OverrideSpellDatafmt[]="niiiiiiiiiixx";
 const char QuestFactionRewardfmt[]="niiiiiiiiii";
@@ -97,37 +97,38 @@ const char Phasefmt[]="nii";
 const char PvPDifficultyfmt[]="diiiii";
 const char RandomPropertiesPointsfmt[]="niiiiiiiiiiiiiii";
 const char ScalingStatDistributionfmt[]="niiiiiiiiiiiiiiiiiiiixi";
-const char ScalingStatValuesfmt[]="iniiiiiiiiiiiiiiiiiiiixxxxxxxxxxxxxxxxxxxxxxxxx";
-const char SkillLinefmt[]="nisxixi";
-const char SkillLineAbilityfmt[]="niiiixxiiiiixx";
-const char SkillRaceClassInfofmt[]="diiiiixxx";
-const char SoundEntriesfmt[]="nissssssssssssssssssssssxxxxxxxxxxx";
+const char ScalingStatValuesfmt[]="iniiiiiixiiiiiiiiiiiiiixxxxxxxxxxxxxxxxxxxxxxxxx"; // 16357
+const char SkillLinefmt[]="nisxixixx"; // 16357
+const char SkillLineAbilityfmt[]="niiiiiiiiixxx"; // 16357
+const char SkillRaceClassInfofmt[]="diiiiixx"; // 16357
+const char SoundEntriesfmt[]="nissssssssssssssssssssssxxxxxxxxxxxx"; // 16357
 const char SpellCastTimefmt[]="niii";
 const char SpellDurationfmt[]="niii";
 const char SpellDifficultyfmt[]="niiii";
-const char SpellEntryfmt[]="niiiiiiiiiiiiiiifiiiissxxiixxixiiiiiiixiiiiiiiix";
-const char SpellAuraOptionsEntryfmt[]="diiii";
-const char SpellAuraRestrictionsEntryfmt[]="diiiiiiii";
+const char SpellEntryfmt[]="nssxxixxxiiiiiiiiiiiiiixi"; // 16357
+const char SpellAuraOptionsEntryfmt[]="dxxiiii"; // 16357
+const char SpellAuraRestrictionsEntryfmt[]="dxxiiiiiiii"; // 16357
 const char SpellCastingRequirementsEntryfmt[]="dixxixi";
-const char SpellCategoriesEntryfmt[]="diiiiii";
+const char SpellCategoriesEntryfmt[]="dxxiiiiiix"; // 16357
 const char SpellClassOptionsEntryfmt[]="dxiiiix";
-const char SpellCooldownsEntryfmt[]="diii";
-const char SpellEffectEntryfmt[]="difiiixfiiiiiifiifiiiiiiiix";
-const char SpellEquippedItemsEntryfmt[]="diii";
-const char SpellInterruptsEntryfmt[]="dixixi";
-const char SpellLevelsEntryfmt[]="diii";
-const char SpellPowerEntryfmt[]="diiiixxx";
-const char SpellReagentsEntryfmt[]="diiiiiiiiiiiiiiii";
-const char SpellScalingEntryfmt[]="diiiiffffffffffi";
-const char SpellShapeshiftEntryfmt[]="dixixx";
-const char SpellTargetRestrictionsEntryfmt[]="dfiiii";
+const char SpellCooldownsEntryfmt[]="dxxiii"; // 16357
+const char SpellEffectEntryfmt[]="dxifiiixfiiiiiifiifiiiiixiiiix"; // 16357
+const char SpellEquippedItemsEntryfmt[]="dxxiii"; // 16357
+const char SpellInterruptsEntryfmt[]="dxxixixi"; // 16357
+const char SpellLevelsEntryfmt[]="dxxiii"; // 16357
+const char SpellPowerEntryfmt[]="xnxiiiiixxxxx"; // 16357
+const char SpellReagentsEntryfmt[]="diiiiiiiiiiiiiiiixx"; // 16357
+const char SpellScalingEntryfmt[]="diiiiffffffffffi"; // FIXME
+const char SpellShapeshiftEntryfmt[]="dixixx"; // 16357
+const char SpellTargetRestrictionsEntryfmt[]="dxxfxiiii"; // 16357
 const char SpellTotemsEntryfmt[]="diiii";
 const char SpellFocusObjectfmt[]="nx";
-const char SpellItemEnchantmentfmt[]="nxiiiiiixxxiiisiiiixxxx";
+const char SpellItemEnchantmentfmt[]="nxiiiiiiiiisiiiixxixxxxxx"; // 16357
 const char SpellItemEnchantmentConditionfmt[]="nbbbbbxxxxxbbbbbbbbbbiiiiiXXXXX";
-const char SpellRadiusfmt[]="nfxx";
+const char SpellMiscfmt[]="dxxiiiiiiiiiiiiiiifiiiii"; // 16357
+const char SpellRadiusfmt[]="nfxxx"; // 16357
 const char SpellRangefmt[]="nffffxxx";
-const char SpellRuneCostfmt[]="niiii";
+const char SpellRuneCostfmt[]="niiixi"; // 16357
 const char SpellShapeshiftFormfmt[]="nxxiixiiixxiiiiiiiixx";
 //const char StableSlotPricesfmt[] = "ni"; // removed
 const char SummonPropertiesfmt[] = "niiiii";
@@ -139,12 +140,12 @@ const char TaxiPathEntryfmt[]="niii";
 const char TaxiPathNodeEntryfmt[]="diiifffiiii";
 const char TotemCategoryEntryfmt[]="nxii";
 const char TransportAnimationEntryfmt[]="diixxxx";
-const char VehicleEntryfmt[]="niffffiiiiiiiifffffffffffffffssssfifiixx";
+const char VehicleEntryfmt[]="nixffffiiiiiiiifffffffffffffffssssfifiixx"; // 16357
 const char VehicleSeatEntryfmt[]="niiffffffffffiiiiiifffffffiiifffiiiiiiiffiiiiixxxxxxxxxxxxxxxxxxxx";
 const char WMOAreaTableEntryfmt[]="niiixxxxxiixxxx";
 const char WorldMapAreaEntryfmt[]="xinxffffixxxxx";
-const char WorldMapOverlayEntryfmt[]="nxiiiixxxxxxxxx";
-const char WorldSafeLocsEntryfmt[]="nifffx";
+const char WorldMapOverlayEntryfmt[]="nxiiiixxxxxxxxxx"; // 16357
+const char WorldSafeLocsEntryfmt[]="nifffxx"; // 16357
 const char WorldPvPAreaEnrtyfmt[]="niiiiii";
 
 #endif

mangosR2/mangos4

Conflicts:
	src/game/Creature.cpp
	src/game/CreatureAI.cpp
	src/game/DBCEnums.h
	src/game/DBCStores.cpp
	src/game/DBCStructure.cpp
	src/game/DBCStructure.h
	src/game/DBCfmt.h
	src/game/ObjectMgr.cpp
	src/game/Spell.cpp
	src/game/SpellAuras.cpp
	src/game/SpellEffects.cpp
	src/game/SpellMgr.cpp
	src/game/SpellMgr.h
	src/game/Totem.cpp
	src/game/Unit.cpp
	src/game/UnitAuraProcHandler.cpp

src/game/DBCfmt.h
@@ -19,25 +19,25 @@
 #ifndef MANGOS_DBCSFRM_H
 #define MANGOS_DBCSFRM_H
 
-const char Achievementfmt[]="niixsxiixixxii";
+const char Achievementfmt[]="niiissiiiiisiix"; // 16357
 const char AchievementCriteriafmt[]="niiiiiiiixsiiiiixxxxxxx";
-const char AreaTableEntryfmt[]="iiinixxxxxisiiiiixxxxxxxxx";
+const char AreaTableEntryfmt[]="iiinixxxxxxxisiiiiixxxxxxxxxxx"; // 16357
 const char AreaGroupEntryfmt[]="niiiiiii";
-const char AreaTriggerEntryfmt[]="nifffxxxfffff";
+const char AreaTriggerEntryfmt[]="nifffxxxfffffxxx"; // 16357
 const char ArmorLocationfmt[]="nfffff";
 const char AuctionHouseEntryfmt[]="niiix";
 const char BankBagSlotPricesEntryfmt[]="ni";
 const char BarberShopStyleEntryfmt[]="nixxxiii";
-const char BattlemasterListEntryfmt[]="niiiiiiiiixsiiiiiiii";
+const char BattlemasterListEntryfmt[]="niiiiiiiiiiixsiiiiiiii"; // 16357
 const char CharStartOutfitEntryfmt[]="diiiiiiiiiiiiiiiiiiiiiiiiixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 const char CharTitlesEntryfmt[]="nxsxix";
 const char ChatChannelsEntryfmt[]="iixsx";
                                                             // ChatChannelsEntryfmt, index not used (more compact store)
-const char ChrClassesEntryfmt[]="nixsxxxixiiiii";
-const char ChrRacesEntryfmt[]="nxixiixixxxxixsxxxxxixxx";
+const char ChrClassesEntryfmt[]="nixsxxxixiiiixxxxx"; // 16357
+const char ChrRacesEntryfmt[]="nxixiixixxxxixsxxxxxixxxxxxxxxxxxxxx"; // 16357
 const char ChrClassesXPowerTypesfmt[]="nii";
 const char CinematicSequencesEntryfmt[]="nxxxxxxxxx";
-const char CreatureDisplayInfofmt[]="nixifxxxxxxxxxxxx";
+const char CreatureDisplayInfofmt[]="nxxifxxxxxxxxxxxxxx"; // 16357
 const char CreatureDisplayInfoExtrafmt[]="nixxxxxxxxxxxxxxxxxxx";
 const char CreatureFamilyfmt[]="nfifiiiiixsx";
 const char CreatureModelDatafmt[] = "nxxifxxxxxxxxxxffxxxxxxxxxxxxxx";
@@ -71,7 +71,7 @@ const char GtSpellScalingfmt[]="df";
 const char GtOCTBaseHPByClassfmt[]="df";
 const char GtOCTBaseMPByClassfmt[]="df";
 const char Holidaysfmt[]="nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-const char ItemClassfmt[]="nixxxs";
+const char ItemClassfmt[]="nxfs"; // 16357
 const char ItemArmorQualityfmt[]="nfffffffi";
 const char ItemArmorShieldfmt[]="nifffffff";
 const char ItemArmorTotalfmt[]="niffff";
@@ -89,9 +89,9 @@ const char LFGDungeonExpansionEntryfmt[] = "niiiiiii";
 const char LiquidTypefmt[] = "nxxixixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 const char LockEntryfmt[]="niiiiiiiiiiiiiiiiiiiiiiiixxxxxxxx";
 const char MailTemplateEntryfmt[]="nxs";
-const char MapEntryfmt[]="nsiixisissififfiixii";
-const char MapDifficultyEntryfmt[]="diisiix";
-const char MovieEntryfmt[]="nxxx";
+const char MapEntryfmt[]="nsiiisissififfiiiii"; // 16357
+const char MapDifficultyEntryfmt[]="niisiis";
+const char MovieEntryfmt[]="nxxxx"; // 16357
 const char MountCapabilityfmt[]="niiiiiii";
 const char MountTypefmt[]="niiiiiiiiiiiiiiiiiiiiiiii";
 const char NumTalentsAtLevelfmt[]="df";
@@ -103,37 +103,38 @@ const char Phasefmt[]="nii";
 const char PvPDifficultyfmt[]="diiiii";
 const char RandomPropertiesPointsfmt[]="niiiiiiiiiiiiiii";
 const char ScalingStatDistributionfmt[]="niiiiiiiiiiiiiiiiiiiixi";
-const char ScalingStatValuesfmt[]="iniiiiiiiiiiiiiiiiiiiixxxxxxxxxxxxxxxxxxxxxxxxx";
-const char SkillLinefmt[]="nisxixi";
-const char SkillLineAbilityfmt[]="niiiixxiiiiixx";
-const char SkillRaceClassInfofmt[]="diiiiixxx";
-const char SoundEntriesfmt[]="nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const char ScalingStatValuesfmt[]="iniiiiiixiiiiiiiiiiiiiixxxxxxxxxxxxxxxxxxxxxxxxx"; // 16357
+const char SkillLinefmt[]="nisxixixx"; // 16357
+const char SkillLineAbilityfmt[]="niiiiiiiiixxx"; // 16357
+const char SkillRaceClassInfofmt[]="diiiiixx"; // 16357
+const char SoundEntriesfmt[]="nissssssssssssssssssssssxxxxxxxxxxxx"; // 16357
 const char SpellCastTimefmt[]="niii";
 const char SpellDurationfmt[]="niii";
 const char SpellDifficultyfmt[]="niiii";
-const char SpellEntryfmt[]="niiiiiiiiiiiiiiifiiiissxxiixxixiiiiiiixiiiiiiiix";
-const char SpellAuraOptionsEntryfmt[]="diiii";
-const char SpellAuraRestrictionsEntryfmt[]="diiiiiiii";
+const char SpellEntryfmt[]="nssxxixxxiiiiiiiiiiiiiixi"; // 16357
+const char SpellAuraOptionsEntryfmt[]="dxxiiii"; // 16357
+const char SpellAuraRestrictionsEntryfmt[]="dxxiiiiiiii"; // 16357
 const char SpellCastingRequirementsEntryfmt[]="dixxixi";
-const char SpellCategoriesEntryfmt[]="diiiiii";
+const char SpellCategoriesEntryfmt[]="dxxiiiiiix"; // 16357
 const char SpellClassOptionsEntryfmt[]="dxiiiix";
-const char SpellCooldownsEntryfmt[]="diii";
-const char SpellEffectEntryfmt[]="difiiiffiiiiiifiifiiiiiiiix";
-const char SpellEquippedItemsEntryfmt[]="diii";
-const char SpellInterruptsEntryfmt[]="dixixi";
-const char SpellLevelsEntryfmt[]="diii";
-const char SpellPowerEntryfmt[]="diiiixxx";
-const char SpellReagentsEntryfmt[]="diiiiiiiiiiiiiiii";
-const char SpellScalingEntryfmt[]="diiiiffffffffffi";
-const char SpellShapeshiftEntryfmt[]="dixixx";
-const char SpellTargetRestrictionsEntryfmt[]="dfiiii";
+const char SpellCooldownsEntryfmt[]="dxxiii"; // 16357
+const char SpellEffectEntryfmt[]="dxifiiiffiiiiiifiifiiiiixiiiix"; // 16357
+const char SpellEquippedItemsEntryfmt[]="dxxiii"; // 16357
+const char SpellInterruptsEntryfmt[]="dxxixixi"; // 16357
+const char SpellLevelsEntryfmt[]="dxxiii"; // 16357
+const char SpellPowerEntryfmt[]="xnxiiiiixxxxx"; // 16357
+const char SpellReagentsEntryfmt[]="diiiiiiiiiiiiiiiixx"; // 16357
+const char SpellScalingEntryfmt[]="diiiiffffffffffi"; // FIXME
+const char SpellShapeshiftEntryfmt[]="dixixx"; // 16357
+const char SpellTargetRestrictionsEntryfmt[]="dxxfxiiii"; // 16357
 const char SpellTotemsEntryfmt[]="diiii";
 const char SpellFocusObjectfmt[]="nx";
-const char SpellItemEnchantmentfmt[]="nxiiiiiixxxiiisiiiixxxx";
+const char SpellItemEnchantmentfmt[]="nxiiiiiiiiisiiiixxixxxxxx"; // 16357
 const char SpellItemEnchantmentConditionfmt[]="nbbbbbxxxxxbbbbbbbbbbiiiiiXXXXX";
-const char SpellRadiusfmt[]="nfxx";
+const char SpellMiscfmt[]="dxxiiiiiiiiiiiiiiifiiiii"; // 16357
+const char SpellRadiusfmt[]="nfxxx"; // 16357
 const char SpellRangefmt[]="nffffxxx";
-const char SpellRuneCostfmt[]="niiii";
+const char SpellRuneCostfmt[]="niiixi"; // 16357
 const char SpellShapeshiftFormfmt[]="nxxiixiiixxiiiiiiiixx";
 //const char StableSlotPricesfmt[] = "ni"; // removed
 const char SummonPropertiesfmt[] = "niiiii";
@@ -145,12 +146,12 @@ const char TaxiPathEntryfmt[]="niii";
 const char TaxiPathNodeEntryfmt[]="diiifffiiii";
 const char TotemCategoryEntryfmt[]="nxii";
 const char TransportAnimationEntryfmt[]="diixxxx";
-const char VehicleEntryfmt[]="niffffiiiiiiiifffffffffffffffssssfifiixx";
+const char VehicleEntryfmt[]="nixffffiiiiiiiifffffffffffffffssssfifiixx"; // 16357
 const char VehicleSeatEntryfmt[]="niiffffffffffiiiiiifffffffiiifffiiiiiiiffiiiiixxxxxxxxxxxxxxxxxxxx";
 const char WMOAreaTableEntryfmt[]="niiixxxxxiixxxx";
 const char WorldMapAreaEntryfmt[]="xinxffffixxxxx";
-const char WorldMapOverlayEntryfmt[]="nxiiiixxxxxxxxx";
-const char WorldSafeLocsEntryfmt[]="nifffx";
+const char WorldMapOverlayEntryfmt[]="nxiiiixxxxxxxxxx"; // 16357
+const char WorldSafeLocsEntryfmt[]="nifffxx"; // 16357
 const char WorldStateEntryfmt[]="niiixxxssiixssiix";
 const char WorldPvPAreaEnrtyfmt[]="niiiiii";
 